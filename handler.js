c3cff91440ad00c7ec65eef05308121feab6be18
Laura Miller <lmiller.spence@gmail.com>
2020-02-10 11:39:22 +0000

    Attempting to hook up to AWS database

--- handler.js
+++ handler.js
@@ -2,18 +2,32 @@
 const uuidv4 = require('uuid/v4');
 const serverless = require('serverless-http');
 const express = require('express');
+const mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'tr-course-rds-instance.ckseo0dq8xc9.eu-west-2.rds.amazonaws.com',
+  user     : 'root',
+  password : 'shh',
+  database : 'tasks'
+});
 const app = express();
 
 app.get('/tasks', function(req, res) {
-  res.json({
-    state: {
-      tasks: [
-        { id: uuidv4(), description: "Walk the dog", completed: false },
-        { id: uuidv4(), description: "Eat cheese", completed: false },
-        { id: uuidv4(), description: "Learn the kazoo", completed: false }
-      ],
-  }
-});
+
+  let tasks = [];
+  console.log('Your tasks are' + tasks);
+
+  connection.query('SELECT taskDescription, status FROM task', function (error, results, fields) {
+    console.log('inside query callback');
+    if (error) throw error;
+    console.log('Query results:' + results);
+    results.forEach(row => {
+      tasks.push({Description:row.taskDescription , Completed:row.status})
+    });
+  });
+
+  res.json(
+    tasks
+);
 });
 
 module.exports.tasks = serverless(app);
