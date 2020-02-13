b32f4c3f4f9df5f4d4e2e91e9a90b1f752035b0b
Laura Miller <lmiller.spence@gmail.com>
2020-02-13 13:30:11 +0000

    Another code update

--- handler.js
+++ handler.js
@@ -1,15 +1,18 @@
 'use strict';
-const uuidv4 = require('uuid/v4');
+
 const serverless = require('serverless-http');
 const express = require('express');
-const mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'tr-course-rds-instance.ckseo0dq8xc9.eu-west-2.rds.amazonaws.com',
-  user     : 'root',
-  password : 'shh',
-  database : 'tasks'
+const app = express();
+const uuidv4 = require('uuid/v4');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host     : 'XXXXXXXXXX',
+  user     : 'XXXXXXXXXXX',
+  password : 'XXXXXXXXXXX',
+  database : 'XXXXXXXXXXX'
 });
-// const app = express();
+
 
 // Retrieving tasks
 app.get('/tasks', function (req, res) {
@@ -48,25 +51,5 @@ app.delete('/tasks/:taskId', function (req, res) {
   });
 });
 
-
-
-// app.get('/tasks', function(req, res) {
-
-//   let tasks = [];
-//   console.log('Your tasks are' + tasks);
-
-//   connection.query('SELECT taskDescription, status FROM task', function (error, results, fields) {
-//     console.log('inside query callback');
-//     if (error) throw error;
-//     console.log('Query results:' + results);
-//     results.forEach(row => {
-//       tasks.push({Description:row.taskDescription , Completed:row.status})
-//     });
-//   });
-
-//   res.json(
-//     tasks
-// );
-// });
 
 module.exports.tasks = serverless(app);
