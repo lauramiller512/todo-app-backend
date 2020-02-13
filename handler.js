a78b127312be7645dad4e264073c291756d51d07
Laura Miller <lmiller.spence@gmail.com>
2020-02-13 12:55:39 +0000

    Updated code

--- handler.js
+++ handler.js
@@ -9,25 +9,64 @@ var connection = mysql.createConnection({
   password : 'shh',
   database : 'tasks'
 });
-const app = express();
+// const app = express();
 
-app.get('/tasks', function(req, res) {
+// Retrieving tasks
+app.get('/tasks', function (req, res) {
 
-  let tasks = [];
-  console.log('Your tasks are' + tasks);
-
-  connection.query('SELECT taskDescription, status FROM task', function (error, results, fields) {
-    console.log('inside query callback');
-    if (error) throw error;
-    console.log('Query results:' + results);
-    results.forEach(row => {
-      tasks.push({Description:row.taskDescription , Completed:row.status})
-    });
+  connection.query('SELECT * FROM `task` WHERE `userId` = "geoff"', function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    if(error) {
+      console.error("Your query had a problem with fetching tasks", error);
+      res.status(500).json({errorMessage: error});
+    }
+    else {
+      // Query was successful
+      res.json({tasks: results});
+    }
   });
-
-  res.json(
-    tasks
-);
 });
+
+// Creating tasks
+app.post('/tasks', function (req, res) {
+  res.json({
+    message: 'Your POST works',
+  });
+});
+
+// Updating tasks
+app.put('/tasks/:taskId', function (req, res) {
+  res.json({
+    message: 'Your PUT works',
+  });
+});
+
+// Deleting tasks
+app.delete('/tasks/:taskId', function (req, res) {
+  res.json({
+    message: 'Your DELETE works',
+  });
+});
+
+
+
+// app.get('/tasks', function(req, res) {
+
+//   let tasks = [];
+//   console.log('Your tasks are' + tasks);
+
+//   connection.query('SELECT taskDescription, status FROM task', function (error, results, fields) {
+//     console.log('inside query callback');
+//     if (error) throw error;
+//     console.log('Query results:' + results);
+//     results.forEach(row => {
+//       tasks.push({Description:row.taskDescription , Completed:row.status})
+//     });
+//   });
+
+//   res.json(
+//     tasks
+// );
+// });
 
 module.exports.tasks = serverless(app);
